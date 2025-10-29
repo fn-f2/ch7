a54c93300e6003db2499f8d5c5dd4fb5560a9e2e
carter <89955530+fn-f2@users.noreply.github.com>
2025-10-29 13:31:59 -0700

    Add files via upload

--- ch7.java
+++ ch7.java
@@ -6,7 +6,7 @@ public class ch7
     
     public static void main(String[] args)
     {
-        System.out.println(indexOfMax(new int[]{-1, -2, -2, -2}));
+        System.out.println(indexOfMax(new int[]{-1, -2, 5, -2}));
         System.out.println(indexOfMax(new int[]{}));
         
         System.out.println("sieve of 50:");
@@ -20,28 +20,27 @@ public class ch7
         System.out.println(areFactors(2, new int[] {2, 4, 7, 16}));       
         System.out.println(areFactors(1, new int[] {}));     
         System.out.println(areFactors(0, new int[] {}));
+        
+        System.out.println(arePrimeFactors(6, new int[] {3, 2}));
     }
     
     public static int indexOfMax(int[] arr)
     {
-        if (arr.length > 0)
+        int max = 0;
+        for (int i = 0; i < arr.length; i++)
         {
-            int max = arr[0];
-            for (int i = 0; i < arr.length; i++)
-            {
-                if (arr[i] > max) max = arr[i];
-            }
-            return max;
-        } else return 0;
+            if (arr[i] > max) max = i;
+        }
+        return max;
     }
     
     public static boolean[] sieve(int n)
     {
         boolean[] ans = new boolean[n];
-        for (int i = 0; i < n; i++)
+        for (int i = 2; i < n; i++)
         {
             ans[i] = true;
-            for (int i2 = 2; i2 < i; i2++)
+            for (int i2 = 2; i2 <= Math.sqrt(i); i2++)
             {
                 if (i2 != i && i % i2 == 0)
                 {
@@ -66,11 +65,18 @@ public class ch7
     
     public static boolean arePrimeFactors(int n, int[] nums)
     {
+        boolean[] numsp = sieve(n);
+        int prod = 1;
+        
         for (int i = 0; i < nums.length; i++)
         {
+            if (!numsp[nums[i]+1]) return false;
+            prod *= nums[i];
+            System.out.println("A" + prod + n);
         }
-     
-        return false;
+        
+        System.out.println("" + prod + n);
+        return (prod == n);
     }
 }
 
